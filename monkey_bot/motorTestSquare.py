ca41884a14b9f1f72719c0d9e4b767db542d2301
pstewart1244 <p.stewart1244@gmail.com>
2026-03-25 22:08:45 -0700

    Add files via upload

---
+++ monkey_bot/motorTestSquare.py
@@ -0,0 +1,32 @@
+# required for motor movement
+import serial
+import time
+from motorAPI import Movement
+
+"""
+Objective: test out the motor movement to ensure that the code corresponds with hardware
+surge moves the robot forward
+yaw turns the robot in place
+"""
+
+monkey_bot = Movement()
+
+monkey_bot.surge(0.3,3) #forward
+monkey_bot.stop(1)
+monkey_bot.yaw(-0.3,3)   #rotate
+monkey_bot.stop(1)      
+
+monkey_bot.surge(0.3,3) #forward
+monkey_bot.stop(1)
+monkey_bot.yaw(-0.3,5)   #rotate
+monkey_bot.stop(1)
+
+monkey_bot.surge(0.3,3) #forward
+monkey_bot.stop(1)
+monkey_bot.yaw(-0.3,5)   #rotate
+monkey_bot.stop(1)
+
+monkey_bot.surge(0.3,3) #forward
+monkey_bot.stop(1)
+
+monkey_bot.close()
